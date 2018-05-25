3b02cbdcc07ce8090ec752e700c8809bd3fa5d18
youxianyen <youxianyen@sina.com>
2018-05-25 18:23:14 +0800

    update at 20180525

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # extjs
+
+
+1.http://extjs.org.cn/node/825
+2.http://extjs.org.cn/trutorial
+3.http://extjs.org.cn/node/829
--- app.js
+++ app.js
@@ -7,4 +7,5 @@ function onSelect(sender, record) {
   var r = record[0].data;
   var text = r.c0+' - '+r.c1+' - '+r.c2;
   alert(text);
+  
 };
